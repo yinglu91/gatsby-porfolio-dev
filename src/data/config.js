f9d43c8300e0295d08d417591e4e451462aa443f
ylu <ylu@ert.com>
2020-11-30 17:52:53 -0500

    init

---
+++ src/data/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  defaultTitle: 'Ying Lu',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'Ying Lu',
+  url: 'https://portfolio.smakosh.com',
+  legalName: 'Ying Lu',
+  defaultDescription: 'I’m Ying and I’m a Backend & Devops engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/smakosh',
+    github: 'https://github.com/smakosh',
+    linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122/',
+    instagram: 'https://instagram.com/smakosh19',
+    youtube: 'https://www.youtube.com/user/smakoshthegamer',
+    google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  // themeColor: '#6b63ff',
+  // backgroundColor: '#6b63ff',
+  themeColor: '#6bffff',
+  backgroundColor: '#6bffff',
+  social: {
+    facebook: 'appId',
+    twitter: '@smakosh',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'USA',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2020',
+};
