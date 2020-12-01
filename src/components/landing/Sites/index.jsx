4bd20337723662c8ed77235ba0df10624bb9506a
ylu <ylu@ert.com>
2020-11-30 22:13:13 -0500

    update image

--- src/components/landing/Sites/index.jsx
+++ src/components/landing/Sites/index.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { ThemeContext } from 'providers/ThemeProvider';
 import { Container, Card, TitleWrap } from 'components/common';
-
 import { Wrapper, Grid, Item, Content, Stats, HvrFloatShadow } from './styles';
 import styles from './sponsor.module.css'
 
@@ -37,27 +36,28 @@ export const Sites = () => {
   return (
     <Wrapper as={Container} id="sites">
       <h2>Sites</h2>
-      <div>
+<Grid>
         {websites.nodes.map(({id, title, url, childScreenshot}) => (
-              <article key={id} className={styles.site}>
-              <a href={url} target="_blank" rel="noopener noreferrer">
-                <div className={styles.hvrFloatShadow}>
-                  <Img
-                    fixed={
-                      childScreenshot.screenshotFile.childImageSharp.fixed
-                    }
-                    alt={title}
-                    className={styles.img}
-                  />
-                </div>
-              </a>
-        
-              <div className={styles.footer}>
-                <h3>{title}</h3>
-              </div>
-            </article>
+          <Item key={id} as="a" href={url} target="_blank" rel="noopener noreferrer" theme={theme}>
+            <Card theme={theme}>
+              <Content className={styles.hvrFloatShadow}>
+                <Img
+                  fixed={
+                    childScreenshot.screenshotFile.childImageSharp.fixed
+                  }
+                  alt={title}
+                  className={styles.img}
+                />
+              </Content>
+              <TitleWrap>
+                <Stats theme={theme}>
+                  <span>{title}</span>
+                </Stats>
+              </TitleWrap>
+            </Card>
+          </Item>
         ))}
-      </div>
+      </Grid>
     </Wrapper>
   );
 };
